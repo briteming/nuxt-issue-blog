f5de65f862a49881bab9d1308239fb850dd912b1
briteming <briteming@gmail.com>
2025-01-03 21:11:54 -0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ module.exports = {
   baseUrl: "/",
   userName: "briteming", // GitHub 用户名
   repository: "nuxt-issue-blog", // 记录 issue 的仓库名
-  accessToken: "Z2hwX05oeUlZd2RwRFl4OU90V2ZhN3VpU1lubkhtbHdwUTNmck9yWg==", // 经过 base64 加密后的 GitHub Token
+  accessToken: "", // 经过 base64 加密后的 GitHub Token
   blogName: "Just a blog", // 给你的博客取个名字
   seo: {
     title: "brite的网络日志",
