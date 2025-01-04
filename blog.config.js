b5eb76a4d17b9a115119c96f58502e695d916e79
briteming <briteming@gmail.com>
2025-01-03 20:58:36 -0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  baseUrl: "/blog/",
-  userName: "yanyue404", // GitHub 用户名
-  repository: "blog", // 记录 issue 的仓库名
-  accessToken: "MWM2YmE5NmMwODJhODgyYzBiZmM2ZWExNGVhNzFhYjFkZTM4MzcwYw==", // 经过 base64 加密后的 GitHub Token
-  blogName: "Just blog and unjust blog", // 给你的博客取个名字
+  baseUrl: "/",
+  userName: "briteming", // GitHub 用户名
+  repository: "nuxt-issue-blog", // 记录 issue 的仓库名
+  accessToken: "Z2hwX05oeUlZd2RwRFl4OU90V2ZhN3VpU1lubkhtbHdwUTNmck9yWg==", // 经过 base64 加密后的 GitHub Token
+  blogName: "Just a blog", // 给你的博客取个名字
   seo: {
-    title: "闫越的网络日志",
+    title: "brite的网络日志",
     description: "Just blog and unjust blog",
     keywords: "基督徒 | F2E",
   },
