0611a7ee650e44a122914d9ba4a09b4cab4f3f00
briteming <briteming@gmail.com>
2025-01-27 11:52:22 -0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ module.exports = {
   baseUrl: "/",
   userName: "briteming", // GitHub 用户名
   repository: "nuxt-issue-blog", // 记录 issue 的仓库名
-  accessToken: "", // 经过 base64 加密后的 GitHub Token
+  accessToken: "github_pat_11AAM4DVY0LNu3oBcVwJLs_kbVl9eVvTpnOc0a"+"EymIHAm7JadCCdlDFsThLF35vrInYSNVBJREPnqV3Kvp", // 经过 base64 加密后的 GitHub Token
   blogName: "Just a blog", // 给你的博客取个名字
   seo: {
     title: "brite的网络日志",
